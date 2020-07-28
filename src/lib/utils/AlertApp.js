11f514c308e2b1a13ff97f4d334dcaa5fa8ef98f
tqlong1609 <tqlong1609@gmail.com>
2020-07-29 01:42:22 +0700

    test confic pull barnch

--- src/lib/utils/AlertApp.js
+++ src/lib/utils/AlertApp.js
@@ -1,11 +1,23 @@
 import {Alert} from 'react-native';
 
 export function AlertApp(title, content, callback) {
+  console.log('888888');
+  console.log('888888');
+  console.log('888888');
+  console.log('888888');
+  console.log('asasd');
   console.log('1123132123');
+  console.log('888888');
+  console.log('888888');
+  console.log('888888');
+  console.log('888888');
+  console.log('asasd');
   console.log('1123132123');
-  console.log('1123132123');
-  console.log('1123132123');
-  console.log('1123132123');
+  console.log('888888');
+  console.log('888888');
+  console.log('888888');
+  console.log('888888');
+  console.log('asasd');
   console.log('1123132123');
   Alert.alert(
     title,
@@ -21,20 +33,20 @@ export function AlertApp(title, content, callback) {
     {cancelable: false},
   );
   console.log('1123132123');
+  console.log('asdasd');
+  console.log('1123132123');
+  console.log('1123132123');
+  console.log('asd');
   console.log('1123132123');
   console.log('1123132123');
   console.log('1123132123');
   console.log('1123132123');
+  console.log('asd');
   console.log('1123132123');
   console.log('1123132123');
   console.log('1123132123');
   console.log('1123132123');
-  console.log('1123132123');
-  console.log('1123132123');
-  console.log('1123132123');
-  console.log('1123132123');
-  console.log('1123132123');
-  console.log('1123132123');
+  console.log('asdasd');
   console.log('1123132123');
   console.log('1123132123');
   console.log('1123132123');
