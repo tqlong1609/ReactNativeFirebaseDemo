a43990d47415ee9257b77ba5c27d41862d5566b9
tqlong1609 <tqlong1609@gmail.com>
2020-07-29 01:19:06 +0700

    test git conflic

--- src/lib/utils/AlertApp.js
+++ src/lib/utils/AlertApp.js
@@ -1,17 +1,35 @@
 import {Alert} from 'react-native';
 
 export function AlertApp(title, content, callback) {
-  Alert.alert(
-    title,
-    content,
-    [
-      {
-        text: 'Cancel',
-        onPress: () => callback('Cancel'),
-        style: 'cancel',
-      },
-      {text: 'OK', onPress: () => callback('OK')},
-    ],
-    {cancelable: false},
-  );
+  console.log('git changele');
+  // Alert.alert(
+  //   title,
+  //   content,
+  //   [
+  //     {
+  //       text: 'Cancel',
+  //       onPress: () => callback('Cancel'),
+  //       style: 'cancel',
+  //     },
+  //     {text: 'OK', onPress: () => callback('OK')},
+  //   ],
+  //   {cancelable: false},
+  // );
+  console.log('git changele');
+
+  console.log('git changele');
+
+  console.log('git changele');
+
+  console.log('git changele');
+
+  console.log('git changele');
+
+  console.log('git changele');
+  console.log('git changele');
+
+  console.log('git changele');
+
+  console.log('git changele');
+
 }
