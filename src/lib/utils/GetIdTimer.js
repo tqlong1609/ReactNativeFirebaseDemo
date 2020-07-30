7a4ed06039425841a89d1a0f92e954a6c2704d57
tqlong1609 <tqlong1609@gmail.com>
2020-07-30 08:27:45 +0700

    test git

--- src/lib/utils/GetIdTimer.js
+++ src/lib/utils/GetIdTimer.js
@@ -1,3 +1,10 @@
 export const getId = () => {
+  console.log('test git');
   return new Date().getTime().toString();
+  console.log('test git');
+
+  console.log('test git');
+
+  console.log('test git');
+
 };
